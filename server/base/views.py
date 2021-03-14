70677d99a7a03271320623c7382d592bc73bf1d2
rochev <rochev_2015@mail.ru>
2021-03-14 11:38:09 +0300

    Фильтры

--- server/base/views.py
+++ server/base/views.py
@@ -1,10 +1,13 @@
 import datetime
 
 # from django.shortcuts import render, HttpResponse
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView, DetailView
 from django.views.generic.list import ListView
 # from django.db.models import Q
-from .models import Manga, Author, Publisher, Painter, TypeManga, Genres
+from .models import (Manga, Author, Publisher,
+                     Painter, TypeManga, Genres,
+                     TitleStatus, TransferStatus)
 
 # from django.views.decorators.csrf import csrf_exempt
 # from django.utils.decorators import method_decorator
@@ -28,21 +31,49 @@ class Data:
     def get_type():
         return TypeManga.objects.all()
 
+    @staticmethod
+    def get_title_status():
+        return TitleStatus.objects.all()
+
+    @staticmethod
+    def get_transfer_status():
+        return TransferStatus.objects.all()
+
 
 class FilterManga(Data, ListView):
+    model = Manga
     template_name = 'catalog.html'
+    paginate_by = 30
+    ordering = ['id']
 
     def get_queryset(self):
         name = self.request.GET.get('name', False)
-        if not name:  # ????????
+        if not name:
             queryset = Manga.objects.filter(
                 genres__in=self.request.GET.getlist('genres', [0, 1, 2, 3]),
                 type_manga__in=self.request.GET.getlist('type', [1, 2, 3, ]),
                 transfer_status__in=self.request.GET.getlist('transfer_status', [0, 1, 2, 3]),
                 title_status__in=self.request.GET.getlist('title_status', [0, 1, 2, 3])).distinct()
+
         else:
             queryset = Manga.objects.filter(title__contains=name)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        get_dict = self.request.GET
+        urls = str()
+        test = dict()
+        if get_dict:
+            for key in get_dict:
+                if key != 'page':
+                    test[key] = [int(item) for item in get_dict.getlist(key)]
+                    for value in get_dict.getlist(key):
+                        urls += f"{key}={value}&"
+
+        context = {'urls': urls,
+                   'test': test}
+
+        return super().get_context_data(**context)
 
 
 class MainTemplateView(TemplateView):
@@ -58,12 +89,15 @@ class MainTemplateView(TemplateView):
 
     def get_context_data(self, **kwargs):
         now = datetime.datetime.now()
-        context = super().get_context_data(**kwargs)
-        context['manga_top'] = Manga.objects.all().order_by("rating")[:20]
-        context['manga_hot'] = Manga.objects.prefetch_related('genres').filter(
+        manga_top = Manga.objects.all().order_by("rating")[:20]
+        manga_hot = Manga.objects.prefetch_related('genres').filter(
             data__range=[now - datetime.timedelta(7), now])[:20]
+        context = {
+            'manga_top': manga_top,
+            'manga_hot': manga_hot
+        }
         # context['chapter_updates'] = Chapter.objects.select_related('manga').all().distinct('manga')[:20]
-        return context
+        return super().get_context_data(**context)
 
 
 #
@@ -86,18 +120,20 @@ class ChapterView(DetailView):
     template_name = 'chapters.html'
 
     def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
         tome = self.object.tome_set.filter(number=kwargs["pk_1"])[0]
-        context['tome'] = tome
         chapter = tome.chapter_set.filter(number=kwargs["pk_2"])[0]
-        context['chapter'] = chapter
-        context['image'] = chapter.chapterimage_set.get(number=kwargs["pk_3"])
-        return context
+        image = chapter.chapterimage_set.get(number=kwargs["pk_3"])
+        context = {
+            'tome': tome,
+            'chapter': chapter,
+            'image': image
+        }
+        return super().get_context_data(**context)
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         context = self.get_context_data(object=self.object, **kwargs)
-        response = self.render_to_response(context)  
+        response = self.render_to_response(context)
         return response
 
 
